ee23506aceed0e146bd63f08f596245064ab1c82
Radi3ntal <amoigus42069@proton.me>
2024-08-10 12:55:10 -0400

    aerew

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,10 +6,39 @@ using UnityEngine.SceneManagement;
 public class PlayerHealth : MonoBehaviour
 {
     public int currentPlayerHealth = 10;
-   
+    public int enemyDamage = 2;
+
+    private Animator playerAnimator;
+    public PlayerExplosionParticles particles;
+
     void Start()
     {
-       
+        playerAnimator = GetComponent<Animator>();
+        particles = GetComponent<PlayerExplosionParticles>();
     }
-    
+
+    public void HurtPlayer()
+    {
+        currentPlayerHealth -= enemyDamage;
+        playerAnimator.SetTrigger("Hit");
+
+        if (currentPlayerHealth <= 0)
+        {
+            particles.Explode();
+            Invoke("ReloadScene", 5);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    { 
+        if (other.tag == "HitCollider")
+        {
+            HurtPlayer();
+        }
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene("CyberFu");
+    }
 }
